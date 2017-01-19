257b6426b702cc4fcf5a511a5f67b69ea967ad2c
Peter <p@6345.uk>
2017-01-19 16:52:21 +0000

    Updating to use the current directory

--- part2and3.py
+++ part2and3.py
@@ -1,22 +1,24 @@
 import time
+import os
 from validator import Validator
 
 if __name__ == '__main__':
+    path = os.getcwd()
     start = time.time()
     print("Part 2 - Bulk import - Begin")
-    validator_part2 = Validator('/home/pslinux/Downloads/import_data.csv')
+    validator_part2 = Validator(path + '/import_data.csv')
     validator_part2.validate()
-    validator_part2.write_csv('/home/pslinux/Downloads/failed_validation.csv', 'failed')
+    validator_part2.write_csv(path + '/failed_validation.csv', 'failed')
     print("Part 2 - Bulk import - End")
     end = time.time()
     print(end - start)
 
     start = time.time()
     print("Part 3 - Performance engineering - Begin")
-    validator_part3 = Validator('/home/pslinux/Downloads/import_data.csv')
+    validator_part3 = Validator(path + '/import_data.csv')
     validator_part3.validate()
-    validator_part3.write_csv('/home/pslinux/Downloads/failed_validation.csv', 'failed')
-    validator_part3.write_csv('/home/pslinux/Downloads/succeeded_validation.csv', 'success')
+    validator_part3.write_csv(path + '/failed_validation.csv', 'failed')
+    validator_part3.write_csv(path + '/succeeded_validation.csv', 'success')
     print("Part 3 - Performance engineering - End")
     end = time.time()
     print(end - start)
